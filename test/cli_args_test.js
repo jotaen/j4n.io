a7bb9c32d7e03923b1dce094690cd18baab56824
Jan Heuermann <jan@jotaen.net>
2016-02-06 11:47:55 +0100

    Use assert in tests

--- test/cli_args_test.js
+++ test/cli_args_test.js
@@ -1,44 +1,71 @@
 "use strict";
 
+const assert = require("assert");
 const args = require("../src/cli_args");
 
 describe("cli_args", () => {
 
-  it("should return default port", () => {
-    const port1 = args.port([]);
-    if (port1 !== 3000) throw new Error();
+  it("should return default port when nothing given", () => {
+    const input  = [];
+    const expect = 3000;
+    const result = args.port(input);
+    assert.equal(expect, result);
+  });
 
-    const port2 = args.port(["--port"]);
-    if (port2 !== 3000) throw new Error();
+  it("should return default port when option (but no value) given", () => {
+    const input = ["--port"];
+    const expect = 3000;
+    const result = args.port(input);
+    assert.equal(expect, result);
+  });
 
-    const port3 = args.port(["--port", "asdf"]);
-    if (port3 !== 3000) throw new Error();
+  it("should return default port when value has wrong format", () => {
+    const input  = ["--port", "asdf"];
+    const expect = 3000;
+    const result = args.port(input);
+    assert.equal(expect, result);
   });
 
   it("should return the port as integer when given", () => {
-    const port = args.port(["--port", "1234"]);
-    if (port !== 1234) throw new Error();
+    const input  = ["--port", "1234"];
+    const expect = 1234;
+    const result = args.port(input);
+    assert.equal(expect, result);
   });
 
-  it("should return if verbose mode is active", () => {
-    const verbose_off = args.verbose([]);
-    if (verbose_off !== false) throw new Error();
-
-    const verbose_on = args.verbose(["--verbose"]);
-    if (verbose_on !== true) throw new Error();
+  it("should return verbose mode inactive by default", () => {
+    const input  = [];
+    const expect = false;
+    const result = args.verbose(input);
+    assert.equal(expect, result);
   });
 
-  it("should return default db host", () => {
-    const host1 = args.db([]);
-    if (host1 !== "mongodb://localhost") throw new Error();
+  it("should return verbose mode inactive by default", () => {
+    const input  = ["--verbose"];
+    const expect = true;
+    const result = args.verbose(input);
+    assert.equal(expect, result);
+  });
 
-    const host2 = args.db(["--db"]);
-    if (host2 !== "mongodb://localhost") throw new Error();
+  it("should return default db host when no option given", () => {
+    const input  = [];
+    const expect = "mongodb://localhost";
+    const result = args.db(input);
+    assert.equal(expect, result);
+  });
+
+  it("should return default db host when option (but no value) given", () => {
+    const input  = ["--db"];
+    const expect = "mongodb://localhost";
+    const result = args.db(input);
+    assert.equal(expect, result);
   });
 
   it("should return the db host when given (protocol prepended)", () => {
-    const host = args.db(["--db", "192.168.15.16/foo"]);
-    if (host !== "mongodb://192.168.15.16/foo") throw new Error();
+    const input  = ["--db", "192.168.15.16/foo"];
+    const expect = "mongodb://192.168.15.16/foo";
+    const result = args.db(input);
+    assert.equal(expect, result);
   });
 
 });
