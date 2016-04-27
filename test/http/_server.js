46b5c27ed48eb03c9de697254b10482206064abd
Jan Heuermann <jan@jotaen.net>
2016-04-27 22:27:40 +0200

    Remove necessity for mocha --delay option

--- test/http/_server.js
+++ test/http/_server.js
@@ -10,16 +10,18 @@ const server = express()
 
 const now = new Date()
 
-db(config.dbUrl, 'shortlinks-system-test-' + now.toISOString()).then((collection) => {
-  const shortlinks = odm(collection)
-  router(server, credentials, shortlinks)
-  run()  /* global run */
-}).catch((error) => {
-  console.log(error)
-  console.log('\n')
-  console.log('>>> In order to run this test, you need to have a mongodb and')
-  console.log('>>> provide the DB_HOST variable to the test process. E.g.')
-  console.log('>>> DB_HOST=localhost:27017 npm test')
+before((done) => {
+  db(config.dbUrl, 'shortlinks-system-test-' + now.toISOString()).then((collection) => {
+    const shortlinks = odm(collection)
+    router(server, credentials, shortlinks)
+    done()
+  }).catch((error) => {
+    console.log(error)
+    console.log('\n')
+    console.log('>>> In order to run this test, you need to have a mongodb and')
+    console.log('>>> provide the DB_HOST variable to the test process. E.g.')
+    console.log('>>> DB_HOST=localhost:27017 npm test')
+  })
 })
 
 module.exports = server
