5059ee00e4d492de354f0442a5c8293e15a56075
Jan Heuermann <jan@jotaen.net>
2016-05-01 16:29:17 +0200

    Add separator in logfile and application start time

--- app/bootstrap/server.js
+++ app/bootstrap/server.js
@@ -4,6 +4,9 @@ const config = require('./config')
 const db = require('./db')
 const app = require('../http/app')
 
+console.log('#'.repeat(40))
+console.log((new Date()).toUTCString())
+
 db.init(config.dbUrl, 'shortlinks').then(() => {
   app.listen(config.port, () => {
     console.log('Server listening on port ' + config.port + '…')
