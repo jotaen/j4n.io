f859940fcbb331e01cc585435bef60cb49ec5128
Jan Heuermann <jan@jotaen.net>
2016-05-05 14:48:44 +0200

    Use shortlink database instead of system

--- app/bootstrap/config.js
+++ app/bootstrap/config.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  dbUrl: 'mongodb://' + (process.env.DB_HOST || '127.0.0.1'),
+  dbUrl: 'mongodb://' + (process.env.DB_HOST || '127.0.0.1') + '/shortlinks',
   port: process.env.PORT || 3000,
   username: process.env.API_USERNAME || 'admin',
   password: process.env.API_PASSWORD || 'a'
