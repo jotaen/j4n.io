5393ec86231612851b4a7bc461c81edbd6597cde
Jan Heuermann <jan@jotaen.net>
2016-05-05 16:56:45 +0200

    Add little inline documentation

--- app/http/redirector.js
+++ app/http/redirector.js
@@ -1,5 +1,7 @@
 'use strict'
 
+// Middleware, that redirects non-authorized* requests to another location
+// (*non-authorized means: requests without authorization header)
 module.exports = (path, redirectUrl) => {
   return (req, res, next) => {
     if (req.path === path && !req.headers.authorization) {
--- app/http/sanitizer.js
+++ app/http/sanitizer.js
@@ -2,6 +2,7 @@
 
 const transform = require('../transform')
 
+// Middleware, that preprocesses/sanitizes request data
 module.exports = () => {
   return (req, res, next) => {
     if (req.params.token) req.params.token = transform.token(req.params.token)
