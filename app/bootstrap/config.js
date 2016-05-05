e7c973ed011d47600d9a30eebe3954b8bc1982a5
Jan Heuermann <jan@jotaen.net>
2016-05-05 18:07:45 +0200

    Add DB variables for production credentials

--- app/bootstrap/config.js
+++ app/bootstrap/config.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  dbUrl: 'mongodb://' + (process.env.DB_HOST || '127.0.0.1') + '/shortlinks',
+  dbUrl: 'mongodb://' + (process.env.DB_USER && process.env.DB_PASSWORD ? process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@' : '') + (process.env.DB_HOST || '127.0.0.1'),
   port: process.env.PORT || 3000,
   username: process.env.API_USERNAME || 'admin',
   password: process.env.API_PASSWORD || 'a'
